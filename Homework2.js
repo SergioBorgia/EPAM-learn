82c57bfe8f3dd7c5bd4cf9ecf212707334d3bbf2
Sergey Antonov <90958539+SergioBorgia@users.noreply.github.com>
2022-10-24 18:25:34 +1300

    Add files via upload

---
+++ Homework2.js
@@ -0,0 +1,24 @@
+<script>
+    let car=();
+    car.colour:"черный";
+    car.colour: "зеленый";
+    let car.power
+    function showpower(){
+        console.log(car.power ?? "неизвестно") 
+    }
+function Frutcount (apples, pears){
+    return apples+pears
+}
+
+function showPrimes(n) {
+  nextPrime: for (let i = 2; i < n; i++) {
+
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+  
+    console.log( i ); 
+}
+}
+
+ </script>
